53956c58639344c24263d834373f2a2e328cd0ca
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2020-07-11 16:28:27 -0300

    Primeiros ajustes...

---
+++ public/app/model/VeiculoModel.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const veiculo = {
+    id: null,
+    data: null,
+    valor: null,
+    marca: null,
+    modelo: null,
+    anoModelo: null,
+    combustivel: null,
+    codigoFipe: null,
+    mesReferencia: null,
+    tipoVeiculo: null,
+    siglaCombustivel: null,
+}
+
+
+
+module.exports = {
+    veiculo
+}
